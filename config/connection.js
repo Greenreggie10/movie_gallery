a77a12f312fea8f9c9c885aa9ad2e20c354bf3ab
Frank Kobe <fkobe3@gmail.com>
2022-09-15 18:49:02 -0400

    heroku issue

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: "127.0.0.1",
       dialect: "mysql",
-      port: 3306,
+      port: 3001,
     });
 
 module.exports = sequelize;
